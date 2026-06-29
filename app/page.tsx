e30c60b4fd27a7c64751fe3afc58a8f6ef554024
Tchango Ayodele Barthelemy <tchangoayodelebarthelemy@gmail.com>
2026-06-29 19:43:06 +0100

    Delete app directory

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{display:'flex', justifyContent:'center', alignItems:'center', height:'100vh', fontSize:'24px'}}>
-      🔥 GeminiAtlas est EN LIGNE sur Vercel
-    </main>
-  )
-}
